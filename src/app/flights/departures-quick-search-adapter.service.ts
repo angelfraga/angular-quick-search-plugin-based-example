0b7f101bb2ad8a55b7d486bd87331dcacbf281b5
angelfraga <angel.fraga.parodi@leanix.net>
2022-02-13 17:39:05 +0100

    Add departures quick search adapater

--- src/app/flights/departures-quick-search-adapter.service.ts
+++ src/app/flights/departures-quick-search-adapter.service.ts
@@ -8,23 +8,20 @@ import {
 @Injectable({
   providedIn: 'root',
 })
-export class ArrivalsQuickSearchAdapterService implements QuickSearchAdapter {
-  private readonly cities: QuickSearchItem[] = [
-    { title: 'RY4700', data: 1 },
-    { title: 'VY2001', data: 2 },
-    { title: 'CA0000', data: 3 },
-    { title: 'RY4700', data: 1 },
-    { title: 'VY2001', data: 2 },
-    { title: 'CA0000', data: 3 },
+export class DeparturesQuickSearchAdapterService implements QuickSearchAdapter {
+  private readonly fligths: QuickSearchItem[] = [
+    { title: 'RY4701', data: 1 },
+    { title: 'VY2002', data: 2 },
+    { title: 'CA0005', data: 3 },
   ];
 
   search(term: string): QuickSearchItem[] | Observable<QuickSearchItem[]> {
-    const items: QuickSearchItem[] = this.cities.filter((item) =>
+    const items: QuickSearchItem[] = this.fligths.filter((item) =>
       item.title.localeCompare(term)
     );
     return of(items).pipe(delay(1800));
   }
   select(item: QuickSearchItem): void {
-    console.log(`/flight/${item.data}`);
+    console.log(`/flights/${item.data}`);
   }
 }
--- src/app/flights/flights.module.ts
+++ src/app/flights/flights.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppSdkModule } from '../app-sdk/app-sdk.module';
 import { ArrivalsQuickSearchAdapterService } from './arrivals-quick-search-adapter.service';
+import { DeparturesQuickSearchAdapterService } from './departures-quick-search-adapter.service';
 
 @NgModule({
   imports: [
@@ -11,6 +12,10 @@ import { ArrivalsQuickSearchAdapterService } from './arrivals-quick-search-adapt
         {
           title: 'Arrivals',
           adapter: ArrivalsQuickSearchAdapterService,
+        },
+        {
+          title: 'Departures',
+          adapter: DeparturesQuickSearchAdapterService,
         },
       ],
     }),
